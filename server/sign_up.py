946518bc0e52a2ac6f16173d6421de7d1da3813d
Mendel Zeev Volf Gorovets <128307863+MZVGorovets@users.noreply.github.com>
2024-07-25 17:43:30 +0300

    added db object to sign_up.py

--- server/sign_up.py
+++ server/sign_up.py
@@ -3,14 +3,15 @@ from message_manipulations import *
 
 
 class Sign_Up():
-    def __init__(self, info, client, key):
+    def __init__(self, info, client, key, db):
+        self.db = db
         self.info = info
         self.client = client
         self.key = key
         password = Message_Manipulations(
             self.key, self.client).receiving_message()
-        if DB().username_exists(self.info):
+        if self.db.username_exists(self.info):
             Message_Manipulations(self.key, self.client).sending_message("400")
         else:
-            DB().insert(self.info, password)
+            self.db.insert(self.info, password)
             Message_Manipulations(self.key, self.client).sending_message("200")
